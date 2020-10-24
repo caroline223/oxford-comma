d6687555a4f1077e3f7c33808ea65ea2c0011a69
caroline223 <carolineforrester22@gmail.com>
2020-10-24 18:15:32 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 def oxford_comma(array)
         
     if array.length == 1
-        array.join(" ") 
+        return array.join(" ") 
     elsif array.length == 2
-        array.join(" and ")
-    elsif array.length == 3
-        array.insert(2, "and")
-        array.join(" , ")
-    elsif array.length > 3
+        return array.join(" and ")
+    elsif array.length >= 3
+        array[-1].prepend "and "
+        array.join(", ")
     end
-
 end
 
